6e8d9ef9f37828b9ca050dbc2ab94fd43ce75d7b
DS Hwang <hds2g22@gmail.com>
2020-06-23 17:36:32 +0900

    Test code for DB write/read

--- src/server.js
+++ src/server.js
@@ -8,6 +8,39 @@ const PORT = process.env.PORT || 4000;
 const server = new GraphQLServer({ schema });
 server.express.use(logger("dev"));
 
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "I like turtles" },
+      },
+    },
+  });
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.disconnect();
+  });
+
 server.start({ port: PORT }, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
 );
